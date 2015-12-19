9ebf8995f7149ced0f85596727567c9a7aac798d
Paul Johnson <paul@johnson.kiwi.nz>
2015-12-19 21:36:11 +1300

    Fix type builders for monad notation.

--- packages/babel-types/src/definitions/experimental.js
+++ packages/babel-types/src/definitions/experimental.js
@@ -1,6 +1,11 @@
 /* @flow */
 
-import defineType, { assertNodeType } from "./index";
+import defineType, {
+  assertNodeType,
+  assertValueType,
+  assertEach,
+  chain
+} from "./index";
 
 defineType("AwaitExpression", {
   builder: ["argument"],
@@ -84,12 +89,21 @@ defineType("MonadNotation", {
   visitor: ["body"],
   aliases: ["Expression"],
   fields: {
+      body: {
+          validate: chain(assertValueType("array"), assertEach(assertNodeType("MonadExpression"))),
+      }
   }
 });
 
 defineType("MonadExpression", {
-  visitor: ["expr"],
-  aliases: ["MonadExpression"],
+  visitor: ["id", "expr"],
   fields: {
+      id: {
+          validate: assertNodeType("Identifier"),
+          optional: true
+      },
+      expr: {
+          validate: assertNodeType("Expression")
+      }
   }
 });
